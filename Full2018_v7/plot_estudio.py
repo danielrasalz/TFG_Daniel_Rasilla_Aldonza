9ecd1b5ac58793ea930c5168c25f5f66fa750c9d
danielrasalz <danielrass04@gmail.com>
2026-07-30 00:07:59 +0200

    Update plot_estudio.py

--- Full2018_v7/plot_estudio.py
+++ Full2018_v7/plot_estudio.py
@@ -1,3 +1,8 @@
+'''
+Agrupación empleada para las figuras de forma
+'''
+
+
 groupPlot = {}
 plot = {}
 
@@ -22,40 +27,96 @@ groupPlot['Other'] = {
     'color': 920,    # Gris
     'samples'  : ['Other','WW', 'VZ' ] 
 
-groupPlot['ttH_ZZ4nu'] = { # O el nombre que le hayas dado
+groupPlot['ttH_ZZ4nu'] = { 
     'nameHR' : 'ttH_ZZ4nu',
-    'isSignal' : 1,      # 2 para que sea una línea
+    'isSignal' : 1,      
     'color'    : 632,    # Rojo (kRed)
-    'samples'  : ['ttH_ZZ4nu'] # Asegúrate de que estos nombres coinciden con samples.py
+    'samples'  : ['ttH_ZZ4nu'] 
 }
 
 
 
-plot['DY'] = {'nameHR':"DY",'color':418,'isSignal':0,'isData':0,'scale':1.0}
+plot['DY'] = {
+  'nameHR':"DY",
+  'color':418,
+  'isSignal':0,
+  'isData':0,
+  'scale':1.0
+}
 
+plot['WW'] = {
+  'nameHR':"WW",
+  'color':851,
+  'isSignal':0,
+  'isData':0,
+  'scale':1.0
+}
 
+plot['VZ'] = {
+  'nameHR':"VZ",
+  'color':851,
+  'isSignal':0,
+  'isData':0,
+  'scale':1.0
+}
 
+plot['ttbar'] = {
+  'nameHR':"t#bar{t}",
+  'color':400,
+  'isSignal':0,
+  'isData':0,
+  'scale':1.0
+}
 
-plot['WW'] = {'nameHR':"WW",'color':851,'isSignal':0,'isData':0,'scale':1.0}
-plot['VZ'] = {'nameHR':"VZ",'color':851,'isSignal':0,'isData':0,'scale':1.0}
+plot['tt_semileptonic'] = {
+  'nameHR':'Semi Leptonic',
+  'color':616,
+  'isSignal':0,
+  'isData':0,
+  'scale':1.0
+}
 
-plot['ttbar'] = {'nameHR':"t#bar{t}",'color':400,'isSignal':0,'isData':0,'scale':1.0}
-plot['tt_semileptonic'] = {'nameHR':'Semi Leptonic','color':616,'isSignal':0,'isData':0,'scale':1.0}
-plot['ttV'] = {'nameHR':'ttV','color':620,'isSignal':0,'isData':0,'scale':1.0}
-plot['SingleTop'] = {'nameHR':'Single top','color':606,'isSignal':0,'isData':0,'scale':1.0}
-plot['Other'] = {'nameHR':'Other','color':920,'isSignal':0,'isData':0,'scale':1.0}
+plot['ttV'] = {
+  'nameHR':'ttV',
+  'color':620,
+  'isSignal':0,
+  'isData':0,
+  'scale':1.0
+}
 
-plot['ttH_ZZ4nu'] = {
-    'nameHR' : 'ttH_ZZ4nu',
+plot['SingleTop'] = {
+  'nameHR':'Single top',
+  'color':606,
+  'isSignal':0,
+  'isData':0,
+  'scale':1.0
+}
+
+plot['Other'] = {
+  'nameHR':'Other',
+  'color':920,
+  'isSignal':0,
+  'isData':0,
+  'scale':1.0
+}
+
+plot['ttH_nonbb'] = {
+    'nameHR' : 'ttH non-bb',
     'color'  : 632,    # Rojo (KRed)
-    'isSignal' : 1,    # 
+    'isSignal' : 2,    
     'isData'   : 0,
-    'scale'    : 100   # ¡IMPORTANTE! Multiplicar por 100 para que se vea en la gráfica,
-                       # si no, será una línea invisible en el suelo.
+    'scale'    : 1.0   
+}
+
+plot['DATA']  = {
+    'nameHR' : 'Data',
+    'color': 1 ,
+    'isSignal' : 0,
+    'isData'   : 1 ,
+    'isBlind'  : 0
 }
 
 
-# --- Opciones adicionales ---
 legend = {}
 legend['lumi'] = 'L = 59.74/fb'
 legend['sqrt'] = '#sqrt{s} = 13 TeV'
